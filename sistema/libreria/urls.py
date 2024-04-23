ad3797c0413f2ff9ceb5cb5e2b12008ca1828368
Jesus Haans Lopez Hernandez <haans@MacBook-Air-de-Jesus.local>
2024-04-22 22:16:19 -0600

    avanzando

---
+++ sistema/libreria/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('bandeja_solicitudes', views.bandeja_solicitudes, name='bandeja_solicitudes'),
+    path('perfil', views.perfil, name='perfil'),
+]
--- sistema/libreria/views.py
+++ sistema/libreria/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def inicio(request):
+    return render(request,"paginas/inicio.html")
+
+
+def bandeja_solicitudes(request):
+    return render(request, "paginas/bandeja_solicitudes.html")
+
+def perfil(request):
+    return render(request, "paginas/perfil.html")
